55f997eac7e30dbebdfdb345aaad5b86e0b8ea72
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-05-11 12:35:31 -0400

    added button to change user name and give a personal greeting

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,23 @@ myImage.onclick =  function() {
     }
 
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Lauren welcomes you, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
